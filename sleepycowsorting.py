55c41ce23610a5b7ccdc55ee6966f8e58061026d
Gautam Dayal <gautam.dayal@yahoo.com>
2019-02-01 19:38:14 +0530

    Create sleepycowsorting.py

---
+++ sleepycowsorting.py
@@ -0,0 +1,29 @@
+"""
+PROB: sleepy
+LANG: PYTHON3
+ID: gautam.8
+"""
+inFile = open('sleepy.in', 'r')
+outFile = open('sleepy.out', 'w')
+
+clist = inFile.readlines()[1]
+cows = [int(i) for i in clist.split()]
+
+def rightMostSort(line):
+    i = len(line) - 1
+    suffLength = 1
+    while 1:
+        
+        if line[i] < line[i - 1]:
+            break
+        elif i == 0:
+            break
+        else:
+            suffLength += 1
+        i -= 1
+    return len(line) - suffLength
+
+outFile.write(str(rightMostSort(cows)))
+outFile.write('\n')
+
+outFile.close()
