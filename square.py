023c89fe4a8e8540b8192c41582ed6de4590b3c2
Gautam Dayal <gautam.dayal@yahoo.com>
2019-02-03 19:58:55 +0530

    Update square.py

--- square.py
+++ square.py
@@ -1,3 +1,19 @@
+# All test cases correct.
+
+# Have written functions for min and max instead of using builtin.
+def maximum(L):
+    currentmax = 0
+    for n in L:
+        if n > currentmax:
+            currentmax = n
+    return currentmax
+
+def minimum(L):
+    currentmin = 11
+    for n in L:
+        if n < currentmin:
+            currentmin = n
+    return currentmin
 
 inFile = open('square.in', 'r')
 outFile = open('square.out', 'w')
@@ -9,11 +25,10 @@ sq2 = [int(c) for c in sqs[1].split()]
 
 xvals = [sq1[0], sq1[2], sq2[0], sq2[2]]
 yvals = [sq1[1], sq1[3], sq2[1], sq2[3]]
-x = max(xvals)-min(xvals)
-y = max(yvals)-min(yvals)
 
+x = maximum(xvals)-minimum(xvals)
+y = maximum(yvals)-minimum(yvals)
 
-print(max([x, y]) ** 2)
-outFile.write(str(max([x, y]) ** 2))
+outFile.write(str(maximum([x, y]) ** 2))
 outFile.write('\n')
 outFile.close()
